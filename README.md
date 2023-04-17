9257c2e7b457fd6da2be8ba7332a4489831a3136
tcdruberu <94827905+tcdruberu@users.noreply.github.com>
2023-04-17 19:09:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# web-page-development-project-2022-
---
+++ buyer.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="buyer12.css">
+
+    <title>buyer</title>
+</head>
+<body>
+<header class="header">
+	<img src="5608-remreview.png">
+    <a href="index.php" class="logo"> </i> ZeroWaste. </a>
+    <header><!-- header start -->
+        <nav class="navbar navbar-expand-lg fixed-top"><!-- nav start -->
+            <div class="container">
+                <a class="navbar-brand" href="index.php">Buyer</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa fa-bars"></i>
+                </button>
+            
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="About Us.php">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="donate.php">Donation</a></li>
+                    <li class="nav-item"><a class="nav-link" href="seller.php">Seller</a></li>
+                    <li class="nav-item"><a class="nav-link" href="web.php">Account</a></li>
+                    
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#life">Life History</a>
+                        <a class="dropdown-item" href="#testimonials">Testimonials</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#gallery">Gallery</a>
+                    </div>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav><!-- nav end -->
+
+        <div class="container header-content w-md-50 w-100">
+        <div class="content">
+          <p class="par">“Those who love quality buy quality products."</p><br><br><br><br><br><br><br><br>Once you have identified products that could be purchased with recycled content through a waste assessment, determine the availability of recycled products that meet your specifications in your area using the resources below, Zerowaste is grate opertunity to start your own bussiness using recycable products.<br>Hurry Up !! And Join Us !!
+
+            </p>
+        
+            <div class="form">
+                <h4>Login Here</h4>
+                <input type="email" name="email" placeholder="Enter Email Here"><br>
+                <input type="password" name="" placeholder="Enter Password Here"><br>
+                <button class="btnn"><a href="log in.php">Login</a></button>
+        </div>
+            </div>    
+    
+    </div>
+            <div class="content">
+                <h2></h2>
+            </div>
+        </div>
+    </header><!-- header end -->
+
+    <div class="section container-fluid about" id="about"><!-- about start -->
+        <div class="row">
+            <div class="col-md-4 col-12">
+                <img src="welc.jpg" alt="Mark Waugh">
+            </div>
+            <div class="col-md-8 col-12">
+                <div class="content">
+                    <h2>Welcome To Our Site</h2>
+                    <p>We understand the importance of being environmentally responsible,<br>and can offer you solutions to do the same.
+                        We always work quickly <br>and offer our services at competitive rates.
+                        Our crew is flexible and willing to bend to meet the needs of our customers.</p>
+                    <p>Would you like to join with us?</p>
+                </div>
+                
+                <div class="about-buttons">
+                    <a href="#contact">Contact Us</a>
+                    <a href="web.php">Sign in</a>
+                </div>
+            </div>
+        </div>
+    </div><!-- about end -->
+
+    <div class="section services" id="services"><!-- service start -->
+        <div class="content1">
+            <div class="container">
+                <h2>What can you do?</h2>
+                <div class="row">
+                    <div class="col-md-4 col-12">
+                        <div class="single-service">
+                            <i class="fa fa-magic" aria-hidden="true"></i>
+                            <h4></h4>
+                            <p>A bottle can be recycled into a BlueStripe cup.
+An unfinished meal to-go can nourish our soils.
+A take-out container with food scraps can be sent to the compost.
+It sounds hard, but it’s our job to figure it out.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-12">
+                        <div class="single-service">
+                            <i class="fa fa-desktop" aria-hidden="true"></i>
+                            <h4></h4>
+                            <p>You don’t need to be an expert to understand.
+Together we can turn the end of a meal into a planet of possibilities.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-12">
+                        <div class="single-service">
+                            <i class="fa fa-code" aria-hidden="true"></i>
+                            <h4></h4>
+                            <p>We believe there’s purpose in our plates.
+There’s consciousness in our cups.
+There’s potential in your humble food scraps.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- service end -->
+
+    <div class="section gallery container" id="gallery"><!-- gallery start -->
+        <h2>What materials can you buy here?</h2>
+        <div class="row images">
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="ply.jpg" alt="Books"><p>Polythene</p></div>
+            </div>
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="glsrm.png" alt="Gallery"><p>Glass</p></div>
+            </div>
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="coco.jpg" alt="Gallery"><p>Coconut Husks</p></div>
+            </div>
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="bkpr.jpg" alt="Gallery"><p>Used Books and Papers</p></div>
+            </div>
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="rmcar.png" alt="Gallery"><p>Used Vehicle Parts</p></div>
+            </div>
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="rmbck1.png" alt="Gallery"><p>clothes</p></div>
+            </div>
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="eldss.jpg" alt="Gallery"><p>used electronic devices</p></div>
+            </div>
+            <div class="imgBox col-md-4 col-12">
+                <div><img src="1.jpg" alt="Gallery"><p>other recycle materials</p></div>
+            </div>
+        </div>
+        <p>would you like to buy such items please contact us!</p>
+         <div class="btn2">
+        <p><a href="web.php">Buy Now</a></p>
+        </div>
+    </div><!-- gallery end -->
+
+    
+    <div class="section contact" id="contact">
+        <div class="container">
+            <h2>Contact Me</h2>
+            <div class="row">
+                <div class="col-lg-6 col-12">
+                    <form>
+                        <div class="form-group">
+                            <input type="text" id="name" required placeholder="Enter your Name">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" id="subject" required placeholder="Enter your Subject">
+                        </div>
+                        <div class="form-group">
+                            <input type="email" id="email" required placeholder="Enter your Email">
+                        </div>
+                        <div class="form-group text-area">
+                            <textarea id="message" required placeholder="Enter Message"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-outline-success">Send Contact Message</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-lg-6 col-12">
+                    <div class="content">
+                        <ul>
+                            <li><a href="#!"><i class="fa fa-map-marker"></i>No:221B,Pitipana Road,Homagama.</a></li>
+                            <li><a href="#!"><i class="fa fa-envelope"></i> zerowaste21.1@gmail.com </a></li>
+                            <li><a href="#!"><i class="fa fa-phone"></i>+7-800-999-800</a></li>
+                            <li><a href="#!"><i class="fa fa-info"></i>Download Resume</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer><!-- footer start -->
+        <div class="section container footer">
+            <div class="footer-social">
+                <ul>
+                    <li><a href="https://www.facebook.com/ZeroWaste-110922141522261"><i class="fa fa-facebook-f"></i></a></li>
+                    <li><a href="https://twitter.com/ZeroWas47282809"><i class="fa fa-twitter"></i></a></li>
+                    <a href="https://github.com/htpathirana/web-page-development-project-2022-"target="blank"> <i class="fab fa-github"></i> github </a>
+                    <li><a href="https://www.instagram.com/zerowaste21.1/"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+            <div class="divider"></div>
+            <div class="footer-bottom">
+                <p>© 2022 WEB PROJECT | Designed by GROUP 3(Ugc.CS)</p>
+            </div>
+        </div>
+    </footer><!-- footer end -->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
